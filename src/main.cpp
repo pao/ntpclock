04aca0b96e470f98338c16494658156844e24fa6
Patrick O'Leary <patrick.oleary@gmail.com>
2024-01-06 18:17:47 -0600

    basics: a complicated serial blinker
    I want to use the SML2 library (header) to represent system states,
    manage connections, and the like. Since there's not a user-controllable
    LED on the wt32-eth01, set up a hacky "blinky" demo instead using a
    serial message. But this acts as a demo of using SML2 to process events.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <string>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "sml2"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+struct toggle{};
+struct button_pressed{};
+
+std::string button_state{"unpressed"};
+
+sml::sm blinker = []
+{
+  auto switch_off = []
+  { Serial.println(("off. button is " + button_state).c_str()); };
+  auto switch_on = []
+  { Serial.println(("ON! button is " + button_state).c_str()); };
+
+  auto set_button = [] { button_state = "pressed"; };
+  auto reset_button = [] { button_state = "reset"; };
+
+  using namespace sml::dsl;
+  return transition_table{
+      *"on"_s + event<toggle> / switch_off = "off"_s,
+      "off"_s + event<toggle> / switch_on = "on"_s,
+
+      *"unpressed"_s + event<button_pressed> / set_button = "pressed"_s,
+      "pressed"_s + event<button_pressed> / reset_button = "unpressed"_s,
+  };
+};
+
+auto interruptPin = 4;
+
+void button() {
+  blinker.process_event(button_pressed{});
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), button, FALLING);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  //Serial.println("Hello world!");
+  blinker.process_event(toggle{});
+  delay(1000);
 }
