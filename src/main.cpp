2c07be6edf6f7eb4448a5800e65e0f40d8972c99
Patrick O'Leary <patrick.oleary@gmail.com>
2024-01-07 20:49:59 -0600

    switch to original sml
    due to the bug in sml2 go back to the older library; get Ethernet example working with state machine

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 #include <string>
 
-#include "sml2"
+#include "sml.hpp"
+namespace sml = boost::sml;
 
 struct network_event {
   WiFiEvent_t ev{};
@@ -17,45 +18,42 @@ auto start_network = [] {
 auto connect_network = [] { Serial.println("ETH Connected"); };
 auto disconnect_network = [] { Serial.println("ETH Disconnected"); };
 
-template <WiFiEvent_t ev>
-bool when(const network_event& net) {
-  return net.ev == ev;
-}
+auto got_ip = [] {
+  Serial.print("ETH MAC: ");
+  Serial.print(ETH.macAddress());
+  Serial.print(", IPv4: ");
+  Serial.print(ETH.localIP());
+  if (ETH.fullDuplex()) {
+    Serial.print(", FULL_DUPLEX");
+  }
+  Serial.print(", ");
+  Serial.print(ETH.linkSpeed());
+  Serial.println("Mbps");
+};
 
-sml::sm sys = [] {
-  using namespace sml::dsl;
-  return transition_table{
-      // clang-format off
-      *"eth_uninitialized"_s + event<network_event>[&when<ARDUINO_EVENT_ETH_START>] / start_network = "eth_disconnected"_s,
-       "eth_disconnected"_s + event<network_event>[&when<ARDUINO_EVENT_ETH_CONNECTED>] / connect_network = "eth_connected"_s,
-       "eth_connected"_s + event<network_event>[&when<ARDUINO_EVENT_ETH_DISCONNECTED>] / disconnect_network = "eth_disconnected"_s,
-       "eth_ready"_s + event<network_event>[&when<ARDUINO_EVENT_ETH_DISCONNECTED>] / disconnect_network = "eth_disconnected"_s,
-      // clang-format on
+template <WiFiEvent_t ev>
+struct when {
+  constexpr bool operator()(const network_event& net) const {
+    return net.ev == ev;
+  }
+};
+
+struct sm_sys {
+  auto operator()() const {
+    using namespace sml;
+    return make_transition_table(
+        // clang-format off
+      *"eth_uninitialized"_s + event<network_event>[when<ARDUINO_EVENT_ETH_START>{}] / start_network = "eth_disconnected"_s
+      ,"eth_disconnected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_CONNECTED>{}] / connect_network = "eth_connected"_s
+      ,"eth_connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "eth_disconnected"_s
+      ,"eth_connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_GOT_IP>{}] / got_ip = "eth_ready"_s
+      ,"eth_ready"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "eth_disconnected"_s
+        // clang-format on
+    );
   };
 };
 
-auto interruptPin = 4;
-
-// void button() { blinker.process_event(button_pressed{}); }
-
-// Ethernet events are handled in the same handler as wifi events
-void dispatch_network_event(WiFiEvent_t event) {
-  switch (event) {
-    // case ARDUINO_EVENT_ETH_GOT_IP:
-    //   Serial.print("ETH MAC: ");
-    //   Serial.print(ETH.macAddress());
-    //   Serial.print(", IPv4: ");
-    //   Serial.print(ETH.localIP());
-    //   if (ETH.fullDuplex()) {
-    //     Serial.print(", FULL_DUPLEX");
-    //   }
-    //   Serial.print(", ");
-    //   Serial.print(ETH.linkSpeed());
-    //   Serial.println("Mbps");
-    //   eth_connected = true;
-    //   break;
-  }
-}
+sml::sm<sm_sys> sys{};
 
 void setup() {
   Serial.begin(9600);
@@ -63,12 +61,8 @@ void setup() {
   WiFi.onEvent([](WiFiEvent_t ev) { sys.process_event(network_event{ev}); });
   ETH.begin();
 
-  pinMode(interruptPin, INPUT_PULLUP);
-  // attachInterrupt(digitalPinToInterrupt(interruptPin), button, FALLING);
+  // pinMode(interruptPin, INPUT_PULLUP);
+  //  attachInterrupt(digitalPinToInterrupt(interruptPin), button, FALLING);
 }
 
-void loop() {
-  // Serial.println("Hello world!");
-  // sys.process_event(toggle{});
-  delay(1000);
-}
+void loop() { delay(1000); }
