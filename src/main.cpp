4a568db50ca37701c2890f25bd770156520d099d
Patrick O'Leary <patrick.oleary@gmail.com>
2024-02-17 16:39:07 -0600

    initial GNSS module interface
    start thinking through GNSS/timing subsystem

--- src/main.cpp
+++ src/main.cpp
@@ -37,27 +37,47 @@ struct when {
   }
 };
 
-sml::sm sys = [] {
+sml::sm connection = [] {
   using namespace sml::dsl;
   return transition_table{
     // clang-format off
-    *"eth_uninitialized"_s + event<network_event>[when<ARDUINO_EVENT_ETH_START>{}] / start_network = "eth_disconnected"_s
-    ,"eth_disconnected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_CONNECTED>{}] / connect_network = "eth_connected"_s
-    ,"eth_connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "eth_disconnected"_s
-    ,"eth_connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_GOT_IP>{}] / got_ip = "eth_ready"_s
-    ,"eth_ready"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "eth_disconnected"_s
+    *"uninitialized"_s + event<network_event>[when<ARDUINO_EVENT_ETH_START>{}] / start_network = "disconnected"_s,
+     "disconnected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_CONNECTED>{}] / connect_network = "connected"_s,
+     "connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "disconnected"_s,
+     "connected"_s + event<network_event>[when<ARDUINO_EVENT_ETH_GOT_IP>{}] / got_ip = "ready"_s,
+     "ready"_s + event<network_event>[when<ARDUINO_EVENT_ETH_DISCONNECTED>{}] / disconnect_network = "disconnected"_s,
     // clang-format on
   };
 };
 
+struct have_fine_time{};
+struct pps_pulse{};
+
+/*
+sml::sm timesync = [] {
+  using namespace sml::dsl;
+  return transition_table{
+    // clang-format off
+    *"init"_s + event<
+    // clang-format on
+  };
+};
+*/
+
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, IO14, IO15);
 
-  WiFi.onEvent([](WiFiEvent_t ev) { sys.process_event(network_event{ev}); });
+  WiFi.onEvent([](WiFiEvent_t ev) { connection.process_event(network_event{ev}); });
   ETH.begin();
 
   // pinMode(interruptPin, INPUT_PULLUP);
   //  attachInterrupt(digitalPinToInterrupt(interruptPin), button, FALLING);
 }
 
-void loop() { delay(1000); }
+void loop() {   
+  delay(100);
+  while(Serial2.available()) {
+    Serial.print(Serial2.readStringUntil('\n') + '\n');
+  }
+}
